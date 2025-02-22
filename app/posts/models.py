98e20a3186fe444f036d52a96483519b79287461
s-ov <s.ovsiuk@gmail.com>
2025-02-22 23:15:47 +0200

    post user field

--- app/posts/models.py
+++ app/posts/models.py
@@ -7,7 +7,7 @@ class Post(db.Model):
     body = db.Column(db.String(180))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # user = db.relationship('User', backref=db.backref('posts', lazy=True))
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
 
     def __repr__(self):
         return f"<Post {self.body}>"
